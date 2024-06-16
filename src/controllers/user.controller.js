39bc0f380676fa69aa886c2e722a153f6eb71a1b
rahulbatham767 <rahulbatham767@gmail.com>
2024-06-16 11:46:13 +0530

    watchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import asyncHandler from "../utils/asyncHandler.js";
 import { uploadCloudinary } from "../utils/cloudinary.js";
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 const generateAccessTokenAndRefreshToken = async (userId) => {
   try {
     const user = await User.findOne(userId);
@@ -213,4 +214,197 @@ export const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
-export const updateUser = asyncHandler(async (req, res) => {});
+export const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req.user?._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+  if (!isPasswordCorrect) {
+    throw new ApiError(401, "Invalid old password");
+  }
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+  res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password Changed successfully"));
+});
+
+export const getCurrentUser = asyncHandler(async (req, res) => {
+  res
+    .status(200)
+    .status(200)
+    .json(200, req.user, "Current User Fetched Successfully");
+});
+
+export const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+  if (!fullName || !email) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const updateUser = await User.findByIdAndUpdate(
+    req.user?._id,
+    { $set: { email, fullName } },
+    { new: true }
+  ).select("-password");
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, updateUser, "Account details updated Successfully")
+    );
+});
+
+export const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(401, "User Avatar is not available");
+  }
+  const avatar = await uploadCloudinary(avatarLocalPath);
+  if (!avatar.url) {
+    throw new ApiError(401, "Error while uploading the avatar");
+  }
+
+  const updateAvatar = await User.findByIdAndUpdate(
+    req.user?._id,
+    { $set: { avatar: avatar.url } },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(200, updateAvatar, "Cover image updated successfully");
+});
+export const updateCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path;
+  if (!coverImageLocalPath) {
+    throw new ApiError(401, "User Avatar is not available");
+  }
+  const coverImage = await uploadCloudinary(coverImageLocalPath);
+  if (!coverImage.url) {
+    throw new ApiError(401, "Error while uploading the avatar");
+  }
+
+  const updateCoverImage = await User.findByIdAndUpdate(
+    req.user?._id,
+    { $set: { avatar: coverImage.url } },
+    { new: true }
+  ).select("-password");
+  return res
+    .status(200)
+    .json(200, updateCoverImage, "Cover image updated successfully");
+});
+
+export const getUserChannnelInfo = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) {
+    throw new ApiError(401, "Username is missing");
+  }
+
+  // now we will match the username in document and take all the document
+  const channel = await User.aggregate([
+    { $match: { username: username?.toLowercase() } },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: { $size: "$subscribers" },
+        channelSubscribedToCount: { $size: "$subscribedTo" },
+        isSubscribed: {
+          $cond: {
+            if: {
+              $in: ["req.user?.id", "$subscribers.subscribe"],
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+  if (!channel?.length) {
+    throw new ApiError(401, "Channel does not exist");
+  }
+  return res
+    .status(200)
+    .json(
+      new apiResponse(200, channel[0], "User channel Fetched Suscessfully")
+    );
+});
+
+export const watchHistory = asyncHandler(async (req, res) => {
+  const user = User.aggregate([
+    // we get string from mongo so we need to convert it to mongo object id
+    { $match: { _id: mongoose.Types.ObjectId(req.user?._id) } },
+    {
+      // finding watch history
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        // to get the owner data we need to lookup for it
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              // for taking only userful data
+              pipeline: [
+                {
+                  $project: {
+                    avatar: 1,
+                    fullName: 1,
+                    username: 1,
+                  },
+                },
+              ],
+            },
+          },
+
+          // simply the owner fields for the user
+
+          {
+            $addFields: {
+              owner: { $first: "$owner" },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(
+      new apiResponse(
+        200,
+        user[0].watchHistory,
+        "watch history fetched successfully  "
+      )
+    );
+});
