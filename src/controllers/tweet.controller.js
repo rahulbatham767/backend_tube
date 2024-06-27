17ce2de7414603d64ec852bb163d6d27c2a75777
rahulbatham767 <rahulbatham767@gmail.com>
2024-06-28 00:42:11 +0530

    fix controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -2,7 +2,7 @@ import mongoose, { isValidObjectId } from "mongoose";
 import { Tweet } from "../models/tweet.model.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/apiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
