1c5514cde490f88ca1439a33b89f23152e15541c
rahulbatham767 <rahulbatham767@gmail.com>
2024-06-28 01:21:23 +0530

    directory update in multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,21 +1,33 @@
 import multer from "multer";
-import path from "path";
+import { GridFsStorage } from "multer-gridfs-storage";
 import fs from "fs";
+import mongoose from "mongoose";
 
 // Ensure the directory exists
-const uploadDir = "./public/temp";
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
+// const uploadDir = "./public/temp";
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir, { recursive: true });
+// }
 
-// Configure multer storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
+const storage = new GridFsStorage({
+  url: process.env.MONGO_URI,
+  options: { useNewUrlParser: true, useUnifiedTopology: true },
+  file: (req, file) => {
+    return {
+      bucketName: "uploads", // Name of the MongoDB collection
+      filename: file.originalname, // Name of the file saved to MongoDB
+    };
   },
 });
+
+// Configure multer storage
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, uploadDir);
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
 
 export const upload = multer({ storage });
