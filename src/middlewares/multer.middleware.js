dc8452ebb188d8a6a05c5ce45cf753154e87fbb0
rahulbatham767 <rahulbatham767@gmail.com>
2024-06-28 15:19:38 +0530

    multer fix

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -3,21 +3,21 @@ import fs from "fs";
 import mongoose from "mongoose";
 
 // Ensure the directory exists
-// const uploadDir = "./public/temp";
+const uploadDir = "./public/temp";
 
-// if (!fs.existsSync(uploadDir)) {
-//   fs.mkdirSync(uploadDir, { recursive: true });
-// }
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // Configure multer storage
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     console.log(req.files.path);
-//     cb(null, uploadDir);
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log(req.files.path);
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+// const storage = multer.memoryStorage();
 export const upload = multer({ storage });
